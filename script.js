b8c8da2766e44af76fcec10441c97130e68a63f5
Alexandre Halas <alexandrehalas@gmail.com>
2023-09-04 15:40:47 -0300

    Converting and checking numbers

--- script.js
+++ script.js
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +251,43 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+// All numbers in javascript internally are represented as floating points
+console.log(43 === 43.0);
+
+// Base 10 - 0 - 9
+// Binary - Base 2 - 0 or 1
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
+
+// Convert strings to numbers
+console.log(Number('23'));
+console.log(+'23');
+
+// Parsing
+console.log(Number.parseInt('56px', 10)); // need starts with number
+console.log(Number.parseInt('a56', 10));
+console.log(Number.parseInt('    56px   ', 10));
+
+console.log(Number.parseInt('2.5rem', 10));
+console.log(Number.parseFloat('2.5rem', 10));
+console.log(parseFloat('2.5rem', 10));
+
+// check if value is a number
+console.log(Number.isNaN(76));
+console.log(Number.isNaN('76'));
+console.log(Number.isNaN(+'76X'));
+console.log(Number.isNaN(76 / 0));
+console.log(76 / 0);
+
+// better way to check if the value is a number
+console.log(Number.isFinite(76));
+console.log(Number.isFinite('76'));
+console.log(Number.isFinite(+'76'));
+console.log(Number.isFinite(76 / 0));
+console.log(Number.isFinite(+'76X'));
+
+console.log(Number.isInteger(58));
+console.log(Number.isInteger(58.0));
+console.log(Number.isInteger(58.9));
+console.log(Number.isInteger(76 / 0));
