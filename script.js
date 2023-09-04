4862db0bfcd3665bd2f5239d3782df6b94af0976
Alexandre Halas <alexandrehalas@gmail.com>
2023-09-04 17:19:49 -0300

    The Remainder Operator

--- script.js
+++ script.js
@@ -296,6 +296,7 @@ console.log(Number.isInteger(58.9));
 console.log(Number.isInteger(76 / 0));
 */
 
+/*
 // MATH AND ROUNDING
 
 console.log('square root');
@@ -380,3 +381,31 @@ console.log((2.7).toFixed(3));
 console.log((2.734).toFixed(2));
 console.log((2.736).toFixed(2));
 console.log(+(2.736).toFixed(2));
+*/
+
+// THE REMAINDER OPERATOR
+
+// simples return the remainder of the division
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
